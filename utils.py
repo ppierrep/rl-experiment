3b938c90b8345715bc35366d8f51f34357179b64
pierrep <pierrepinchon974@gmail.com>
2020-06-05 18:00:37 +0200

    update main

--- utils.py
+++ utils.py
@@ -2,13 +2,21 @@ from IPython.display import clear_output
 import matplotlib.pyplot as plt
 
 
-def plot(rewards, losses):
+def plot(rewards, losses, action_takens):
     clear_output(True)
     plt.figure(figsize=(20,5))
+
     plt.subplot(131)
     plt.title('rewards')
     plt.plot(rewards)
+
     plt.subplot(132)
     plt.title('loss')
     plt.plot(losses)
+
+    plt.subplot(133)
+    unique, counts = np.unique(action_takens, return_counts=True)
+    plt.bar(unique, counts/np.sum(counts))
+    plt.title("Action distribution")
+
     plt.show()
